354e24bbad07bd44f8d04920bd51c7976cb925ea
maxcodebuilder <maxwell@geniusworks.com>
2026-07-30 14:59:56 -0700

    Made a madlib game

---
+++ games/Game2/madlib.py
@@ -0,0 +1,38 @@
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+resturant = input("Resturant: ")
+country = input("Country: ")
+book_title = input("Book title: ")
+adj2 = input("Adjective: ")
+item = input("Item: ")
+adj3 = input("Adjective: ")
+name = input("Name: ")
+noun = input("Noun: ")
+adj4 = input("Adjective: ")
+place = input("Place: ")
+verb3 = input("Verb: ")
+country2 = input("Country: ")
+number = input("Number: ")
+name2 = input("Name: ")
+noun2 = input("Noun: ")
+adj5 = input("Adjective: ")
+adj6 = input("Adjective")
+food = input("Food: ")
+adj7 = input("Adjective: ")
+verb4 = input("Verb: ")
+number2 = input("Number: ")
+growth = input("Kid/Adult/Grandparent: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}! I like to eat \
+at {resturant}. It is the best place in {country}. My favorite book is {book_title} \
+because it is {adj2}. I wish I had a(n) {item} because it is {adj3}. My name is {name} \
+and I like to {noun} because it is {adj4}. My favorite place to go to is {place} and in \
+this place I like to {verb3}. (This place is in {country2}) I have {number} pets, if I \
+could, I would name one {name2}. The best thing in the world is {noun2} because it is \
+{adj5} and {adj6}. My favorite food is {food} because it is {adj7}. I like to {verb4}. \
+I am {number2} years old and I am a(n) {growth}."
+
+print(madlib)
