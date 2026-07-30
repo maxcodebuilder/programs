4ec30f9e4d04ba1b5f28659b8c5b2dfafc401640
maxcodebuilder <maxwell@geniusworks.com>
2026-07-30 15:33:20 -0700

    added more stuff tp new game

--- games/Game3/Game3.py
+++ games/Game3/Game3.py
@@ -1,2 +1,8 @@
 print("Welcome to my game!")
+name = input("What is your name? ")
+age = input("What is your age? ")
 
+print("Hello", name, "you are", age, "years old")
+
+is_older = int(age) >= 5
+print(is_older)
