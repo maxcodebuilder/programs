b308393a238a1c8ce4aa92e2afba2814d87adb96
maxcodebuilder <maxwell@geniusworks.com>
2026-08-03 13:27:21 -0700

    MORE STUFF

--- games/Game4/Game4.py
+++ games/Game4/Game4.py
@@ -3,8 +3,8 @@ birth_date = input ("Is it your birthday today? ")
 year = input ("What year is it? ")
 birth_year = input ("What year were you born? ")
 age = int(year) - int(birth_year)
-print ("Hello, " + name + "! You are " + str(age) + " years old.")
+print ("Hello, ", name, "! You are ", str(age), " years old.")
 if birth_date == "yes":
-    print ("Happy birthday, " + name + "!")
+    print ("Happy birthday, ", name, "!")
 if birth_date == "no":
     print ("Oh, okay.")
