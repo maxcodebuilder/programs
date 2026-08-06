52a83b104737f14b023e0c096634d13006eb3345
maxcodebuilder <maxwell@geniusworks.com>
2026-08-06 09:38:05 -0700

    Made a rock paper scissors game!

--- games/Game21/Game21.py
+++ games/Game21/Game21.py
@@ -1,17 +1,32 @@
-# Simple Pong in Python 3 for Beginners
-# By Maxwell Diekhoff
-# Part 1: Getting started
+import random
 
-import turtle
+def get_choices():
+    player_choice = input("Enter a choice (rock, paper, scissors): ")
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
+    choices = {"player": player_choice, "computer": computer_choice}
+    return choices
 
-wn = turtle.Screen()
-wn.title("Pong By Maxwell Diekhoff")
-wn.bgcolor("black")
-wn.setup(width=800, height=600)
-wn.tracer(0)
+def check_win(player, computer):
+    print(f"You chose {player} computer chose {computer}")
+    if player == computer:
+        return "It's a tie"
+    elif player == "rock":
+        if computer == "scissors":
+            return "You win!"
+        else:
+            return "You lose..."
+    elif player == "paper":
+        if computer == "rock":
+            return "You win!"
+        else:
+            return "You lose..."
+    elif player == "scissors":
+        if computer == "paper":
+            return "You win!"
+        else:
+            return "You lose..."
 
-
-
-# Main game loop
-while True:
-    wn.update()
+choices = get_choices()
+result = check_win(choices["player"], choices["computer"])
+print(result)
