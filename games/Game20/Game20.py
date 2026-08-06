33f2e31b1d3c27dc0320b7a8c71e92f756adb19a
maxcodebuilder <maxwell@geniusworks.com>
2026-08-06 08:31:41 -0700

    Started fixing bg color

--- games/Game20/Game20.py
+++ games/Game20/Game20.py
@@ -1,9 +1,11 @@
 from tkinter import *
 
-
 root = Tk()
-root.configure(bg="black")
 root.geometry("1440x720")
 root.title("Minesweeper Game")
 root.resizable(False, False)
+
+bg_frame = Frame(root, bg="black")
+bg_frame.pack(fill=BOTH, expand=True)
+
 root.mainloop()
--- games/Game21/Game21.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-
-
-root = Tk()
-root.configure(bg="black")
-root.geometry("1440x720")
-root.title("Minesweeper Game")
-root.resizable(False, False)
-root.mainloop()
